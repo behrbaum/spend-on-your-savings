72be9e99ab4864e26a43a552c0d8163c45feffdf
Elena Iaconis <eiaconis@seas.upenn.edu>
2018-11-05 19:52:11 -0500

    basic app

---
+++ Team6SeniorDesign/Team6SeniorDesign/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Team6SeniorDesign
+//
+//  Created by Elena Iaconis on 11/5/18.
+//  Copyright © 2018 Team 20. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
