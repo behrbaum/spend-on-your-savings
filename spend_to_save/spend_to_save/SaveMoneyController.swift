abc51d55e4f49d2751cfdcbe869666add3e9cf44
OIDUser <oiduser@OIDUsers-MacBook-Air.local>
2018-11-07 23:54:34 -0500

    MVP Demo

---
+++ spend_to_save/spend_to_save/SaveMoneyController.swift
@@ -0,0 +1,33 @@
+//
+//  SaveMoneyController.swift
+//  spend_to_save
+//
+//  Created by OIDUser on 11/6/18.
+//  Copyright © 2018 OIDUser. All rights reserved.
+//
+
+import UIKit
+
+class SaveMoneyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        print("save button pressed")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ spend_to_save/spend_to_save/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spend_to_save
+//
+//  Created by OIDUser on 11/6/18.
+//  Copyright © 2018 OIDUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //@IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //loginButton.layer.borderWidth = 1
+        //loginButton.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        print("login button pressed")
+    }
+}
+
