3201849283840a963fdfeb8d6262e905416ee557
Elena Iaconis <eiaconis@seas.upenn.edu>
2018-11-27 08:17:07 -0500

    setup AWS backend

---
+++ spend_to_save/Pods/Target Support Files/Pods-spend_to_save/Pods-spend_to_save-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_spend_to_save : NSObject
+@end
+@implementation PodsDummy_Pods_spend_to_save
+@end
---
+++ spend_to_save/Pods/Target Support Files/Pods-spend_to_saveTests/Pods-spend_to_saveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_spend_to_saveTests : NSObject
+@end
+@implementation PodsDummy_Pods_spend_to_saveTests
+@end
