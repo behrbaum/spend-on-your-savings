7975955907953608ca2dfd6ac65f6e90a553fc41
OIDUser <oiduser@OIDUsers-MacBook-Air.local>
2018-12-06 15:08:21 -0500

    Update App flow and add savings categories.  Start menu items

---
+++ spend_to_save/spend_to_save/ManualSaveMoneyViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ManualSaveMoneyViewController.swift
+//  spend_to_save
+//
+//  Created by OIDUser on 12/6/18.
+//  Copyright © 2018 OIDUser. All rights reserved.
+//
+
+import UIKit
+
+class ManualSaveMoneyViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var categoryLabel: UITextField!
+    
+    var categories = ["Food", "Transportation", "Entertainment", "Rent", "Shopping"]
+    var picker = UIPickerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        picker.dataSource = self
+        categoryLabel.inputView = picker
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryLabel.text = categories[row]
+    }
+
+}
---
+++ spend_to_save/spend_to_save/MenuItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuItemViewController.swift
+//  spend_to_save
+//
+//  Created by OIDUser on 12/6/18.
+//  Copyright © 2018 OIDUser. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let menu = ["Small Coffee", "Medium Coffee", "Large Coffee"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = menu[indexPath.row]
+        return cell
+    }
+
+}
